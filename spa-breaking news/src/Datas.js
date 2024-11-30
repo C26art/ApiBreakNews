144277b66c62e6e92048845545d0dd7b34f3bec6
Cristian Mitidieri <cristianmc100@gmail.com>
2024-11-29 23:45:49 -0300

    feat: create Data Mock

---
+++ spa-breaking news/src/Datas.js
@@ -0,0 +1,30 @@
+export const news = [
+    {
+        title: "Noticias 1",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto minima voluptatibus consequuntur, nostrum nulla repellat enim quo perferendis dolore, nihil vitae, nemo quisquam omnis. Voluptas, rerum perspiciatis optio praesentium a veritatis ipsa, corporis in ut sed earum ipsam at officia.",
+        image: "https://images.unsplash.com/photo-1683383409763-2b86f821797f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODN8fG5ld3N8ZW58MHwwfDB8fHww",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "Noticias 2",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto minima voluptatibus consequuntur, nostrum nulla repellat enim quo perferendis dolore, nihil vitae, nemo quisquam omnis. Voluptas, rerum perspiciatis optio praesentium a veritatis ipsa, corporis in ut sed earum ipsam at officia.",
+        image: "https://images.unsplash.com/photo-1557089041-7fa93ffc2e08?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTF8fG5ld3N8ZW58MHwwfDB8fHww",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "Noticias 3",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto minima voluptatibus consequuntur, nostrum nulla repellat enim quo perferendis dolore, nihil vitae, nemo quisquam omnis. Voluptas, rerum perspiciatis optio praesentium a veritatis ipsa, corporis in ut sed earum ipsam at officia.",
+        image: "https://images.unsplash.com/photo-1606318524267-121fa68eea7b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTR8fG5ld3N8ZW58MHwwfDB8fHww",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "Noticias 4",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto minima voluptatibus consequuntur, nostrum nulla repellat enim quo perferendis dolore, nihil vitae, nemo quisquam omnis. Voluptas, rerum perspiciatis optio praesentium a veritatis ipsa, corporis in ut sed earum ipsam at officia.",
+        image: "https://images.unsplash.com/photo-1709285671867-245153973b14?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTAyfHxuZXdzfGVufDB8MHwwfHx8MA%3D%3D",
+        likes: 2,
+        comments: 4
+    },
+]
